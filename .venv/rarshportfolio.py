42f59c6b5efc8936c9ca2c4cd9dac91460462199
rarsh-viral <145724814+rarsh-viral@users.noreply.github.com>
2024-02-14 01:48:56 +0100

    all my portfolio files
    all my project first commit

---
+++ .venv/rarshportfolio.py
@@ -0,0 +1,34 @@
+import csv
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def myhomepage():
+    return render_template("index.html")
+
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_file(data):
+    with open("database.csv", mode="a") as database:
+        name = data["name"]
+        email = data["email"]
+        message = data["message"]
+        csv_writer= csv.writer(database, delimiter=",", quotechar="'",quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([name,email,message])
+
+@app.route("/submit_form", methods=["POST","GET"])
+def submit_form():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        write_to_file(data)
+        return render_template("thanku.html")
+    else:
+        return "something went wrong"
+
+
+if __name__ == "__main__":
+    app.run()
